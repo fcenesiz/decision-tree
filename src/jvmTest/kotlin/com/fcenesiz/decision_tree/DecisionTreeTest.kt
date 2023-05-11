c15b112627e268a775c4a5368d18585d35a6a45c
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2023-05-11 19:27:25 +0300

    adding gradlew

---
+++ src/jvmTest/kotlin/com/fcenesiz/decision_tree/DecisionTreeTest.kt
@@ -0,0 +1,11 @@
+package com.fcenesiz.decision_tree
+
+import org.junit.jupiter.api.Test
+
+class DecisionTreeTest {
+
+    @Test
+    fun testTree(){
+        val decisionTree = DecisionTree()
+    }
+}
